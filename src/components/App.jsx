2bd5db3cce7d1e5eb93021406dae9230b0ba9887
avnishpandey113 <avnishpandey113@live.com>
2020-11-19 21:07:33 -0600

    Add note component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import notes from '../notes';
 import Header from './Header';
 import Note from './Note';
 import Footer from './Footer';
 
 function App() {
-  return (<div><Header /> <Note /> <Footer /></div>);
+  return (
+    <div>
+      <Header />
+      {notes.map(note => 
+        <Note key={note.id}
+          title={note.title}
+          content={note.content} />
+      )}
+      <Footer />
+    </div>);
 }
 
 export default App;
